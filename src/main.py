0a766cbe1a364a72a988e6a7602b456a38f1b3fb
R31V4X <johnson.gd007@gmail.com>
2022-12-11 16:55:17 -0500

    added tabs

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.ttk import *
 
 # Setup
 root = Tk()
@@ -8,6 +9,39 @@ WIN_WIDTH, WIN_HEIGHT = 1000, 800
 root.geometry(F"{WIN_WIDTH}x{WIN_HEIGHT}+{int((root.winfo_screenwidth()/2)-(WIN_WIDTH/2))}+{int((root.winfo_screenheight()/2)-(WIN_HEIGHT/2))}")
 root.title("DIREKTO")
 root.resizable(False, False)
+
+
+# Tabs
+notebook = Notebook(root)
+notebook.pack(pady=25)
+
+
+tab1 = Frame(notebook, width=WIN_WIDTH-50, height=WIN_HEIGHT-50)
+tab1.pack()
+
+tab2 = Frame(notebook, width=WIN_WIDTH-50, height=WIN_HEIGHT-50)
+tab2.pack()
+
+tab3 = Frame(notebook, width=WIN_WIDTH-50, height=WIN_HEIGHT-50)
+tab3.pack()
+
+notebook.add(tab1, text="Directory Management")
+notebook.add(tab2, text="File Management")
+notebook.add(tab3, text="Settings")
+
+
+
+#Tab1
+
+#   Add sequence of directories
+#
+#
+#
+#
+
+
+add_seq_dir_frame = LabelFrame(tab1, text="Add Sequence of Directories", width=100, height=100)
+add_seq_dir_frame.pack()
 
 
 root.mainloop()
